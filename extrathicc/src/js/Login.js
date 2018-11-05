38cd8a46d04f4fbc24598a31ee338ef9c131a005
Hao Dang <hdang32@gatech.edu>
2018-11-04 23:19:00 -0500

    reroute login to homepage

--- extrathicc/src/js/Login.js
+++ extrathicc/src/js/Login.js
@@ -55,7 +55,7 @@ class Login extends Component {
 
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to="/" />;
+      return <Redirect to="/home" />;
     }
   };
 
