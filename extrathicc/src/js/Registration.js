0c49e68910de544b3f58019413958799794cf38c
Hao Dang <hdang32@gatech.edu>
2018-11-08 17:29:13 -0500

    modify password length

--- extrathicc/src/js/Registration.js
+++ extrathicc/src/js/Registration.js
@@ -53,7 +53,7 @@ class Registration extends Component {
 
     validateForm() {
         if (this.state.username.length > 0
-            && this.state.password.length > 0
+            && this.state.password.length > 8
             && this.state.email.length > 0
             && this.state.confirm.length > 0) {
             if (!this.state.email.includes('@')) {
