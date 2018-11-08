b94d8428fc1946d663ce30cce591bc8f44947c7c
Hao Dang <hdang32@gatech.edu>
2018-11-08 17:59:21 -0500

    add staff home page ui

--- extrathicc/src/Routes.js
+++ extrathicc/src/Routes.js
@@ -9,10 +9,13 @@ import ExhibitDetail from "./js/ExhibitDetail";
 import ShowHistory from "./js/ShowHistory";
 import Shows from "./js/Shows";
 import Animals from "./js/Animals";
+import AssignedShows from "./js/AssignedShows";
+import StaffHome from "./js/StaffHome";
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path="/staffhome" component={StaffHome} />;
       <Route path="/home" component={HomePage} />;
       <Route path="/login" component={Login} />;
       <Route path="/registration" component={Registration} />;
@@ -22,6 +25,7 @@ const Routes = () => {
       <Route path="/exhibit_history" component={ExhibitHistory} />;
       <Route path="/shows_history" component={ShowHistory} />;
       <Route path="/exhibits_id" component={ExhibitDetail} />;
+      <Route path="/assigned/shows" component={AssignedShows} />;
     </Switch>
   );
 };
---
+++ extrathicc/src/js/AssignedShows.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "../css/Login.css";
+
+class AssignedShows extends Component {
+
+}
+
+export default AssignedShows;
