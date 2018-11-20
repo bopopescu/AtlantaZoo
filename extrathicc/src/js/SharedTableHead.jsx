bf3aaa7ec8aabd6770b555db659bfc6a97dd6923
Hao Dang <hdang32@gatech.edu>
2018-11-19 23:32:32 -0500

    set up tables for Animals

---
+++ extrathicc/src/js/SharedTableHead.jsx
@@ -0,0 +1,72 @@
+import TableHead from '@material-ui/core/TableHead';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/core/Checkbox';
+import TableCell from '@material-ui/core/TableCell';
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Tooltip from '@material-ui/core/Tooltip';
+
+class SharedTableHead extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    createSortHandler = property => event => {
+        this.props.onRequestSort(event, property);
+    };
+
+    render() {
+        const { onSelectAllClick, order, orderBy, numSelected, rowCount, data } = this.props;
+
+        return (
+            <TableHead>
+                <TableRow>
+                    <TableCell padding="checkbox">
+                        <Checkbox
+                            indeterminate={numSelected > 0 && numSelected < rowCount}
+                            checked={numSelected === rowCount}
+                            onChange={onSelectAllClick}
+                        />
+                    </TableCell>
+                    {data.map(row => {
+                        return (
+                            <TableCell
+                                key={row.id}
+                                numeric={row.numeric}
+                                padding={row.disablePadding ? 'none' : 'default'}
+                                sortDirection={orderBy === row.id ? order : false}
+                            >
+                                <Tooltip
+                                    title="Sort"
+                                    placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                                    enterDelay={300}
+                                >
+                                    <TableSortLabel
+                                        active={orderBy === row.id}
+                                        direction={order}
+                                        onClick={this.createSortHandler(row.id)}
+                                    >
+                                        {row.label}
+                                    </TableSortLabel>
+                                </Tooltip>
+                            </TableCell>
+                        );
+                    }, this)}
+                </TableRow>
+            </TableHead>
+        );
+    }
+}
+
+SharedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.string.isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+    data: PropTypes.string.isRequired,
+};
+
+export default SharedTableHead;
