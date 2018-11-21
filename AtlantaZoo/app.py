cd7174bac44028ce2c61503111f4e26d0e262581
quyentran <btran34@gatech.edu>
2018-11-21 15:39:17 -0500

    fixed the login to email, and added the log note

--- AtlantaZoo/app.py
+++ AtlantaZoo/app.py
@@ -9,14 +9,17 @@ CORS(app, supports_credentials=True)
 
 app.secret_key = 'super secret key'
 
+
 @app.route('/login', methods=['POST'])
 def login():
     return jsonify(message=helpers.login(request.json['email'], request.json['password']))
 
-#just for testing who is logged in
+
+# just for testing who is logged in
 @app.route('/whoami', methods=['GET'])
 def whoami():
     return jsonify(username=session['username'])
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -25,15 +28,16 @@ def create_user():
                                                request.json['password'],
                                                request.json['user_type']))
 
+
 @app.route('/staff', methods=['GET'])
 def get_all_staff():
     return jsonify(message=helpers.get_all_staff())
+
 
 @app.route('/logout', methods=['GET'])
 def logout():
     session.clear()
     return jsonify(message='You have successfully logged out')
-
 
 
 @app.route('/animals', methods=['POST'])
@@ -44,11 +48,13 @@ def create_animal():
                                                  request.json['age'],
                                                  request.json['exhibit_name']))
 
+
 @app.route('/exhibits', methods=['POST'])
 def create_exhibit():
     return jsonify(message=helpers.create_exhibit(request.json['exhibit_name'],
                                                   request.json['water_feature'],
                                                   request.json['size']))
+
 
 @app.route('/shows', methods=['POST'])
 def create_show():
@@ -57,9 +63,11 @@ def create_show():
                                                request.json['staff_name'],
                                                request.json['exhibit_name']))
 
+
 @app.route('/shows', methods=['GET'])
 def get_all_shows():
     return jsonify(message=helpers.get_all_shows())
+
 
 @app.route('/animals', methods=['GET'])
 def get_all_animals():
@@ -68,20 +76,45 @@ def get_all_animals():
     if name and species:
         return jsonify(message=helpers.get_animal(name, species))
 
-    return jsonify(message=helpers.get_all_animals())  
+    return jsonify(message=helpers.get_all_animals())
+
 
 @app.route('/animals/<animal_name>/<species>', methods=['DELETE'])
 def delete_animal(animal_name, species):
     return jsonify(message=helpers.delete_animal(animal_name, species))
 
+
 @app.route('/shows/<show_name>/<show_time>', methods=['DELETE'])
 def delete_show(show_name, show_time):
     return jsonify(message=helpers.delete_show(show_name, show_time))
+
 
 @app.route('/users/<username>', methods=['DELETE'])
 def delete_user(username):
     return jsonify(message=helpers.delete_user(username))
 
+
+# log_exhibit_visit
+@app.route('/visit_exhibit', methods=['POST'])
+def log_exhibit_visit():
+    return jsonify(message=helpers.log_exhibit_visit(request.json['visitor_username'],
+                                                     request.json['exhibit_name'],
+                                                     request.json['visit_time']))
+
+#log_show_visit
+@app.route('/visit_show', methods=['POST'])
+def log_show_visit():
+    return jsonify(message=helpers.log_show_visit(request.json['visitor_username'],
+                                                  request.json['show_name'],
+                                                  request.json['show_time']))
+#log_note
+@app.route('/notes', methods=['POST'])
+def log_note():
+    return jsonify(message=helpers.log_note(request.json['staff_username'],
+                                            request.json['log_time'],
+                                            request.json['note'],
+                                            request.json['animal_name'],
+                                            request.json['animal_species']))
 
 @app.errorhandler(HTTPException)
 def general_errors(e: HTTPException):
@@ -91,6 +124,7 @@ def general_errors(e: HTTPException):
 @app.errorhandler(Exception)
 def general_errors(e):
     return make_response(jsonify(message=repr(e)), 500)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- AtlantaZoo/helpers.py
+++ AtlantaZoo/helpers.py
@@ -47,16 +47,16 @@ def create_user(username, email, password, user_type):
     else:
         abort(400, message="Duplicate user given")
 
-def login(username, password):
+def login(email, password):
     conn, curr = connection()
 
-    curr.execute('SELECT * FROM User where username = %s and password = %s', (username, password))
+    curr.execute('SELECT * FROM User where email = %s and password = %s', (email, password))
     row = curr.fetchone()
     if row is None:
         abort(401, message="Incorrect username or password")
 
     session['logged_in'] = True
-    session['username'] = username
+    session['email'] = email
 
     curr.close()
     conn.close()
@@ -135,6 +135,10 @@ def get_all_exhibits():
     curr.execute("SELECT * FROM Exhibit")
 
     results = curr.fetchall()
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 # gets size, exhibit name, and water feature
@@ -146,6 +150,9 @@ def get_exhibit_details(exhibit):
 
     results = curr.fetchall()
 
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_exhibit_animals(exhibit):
@@ -155,6 +162,9 @@ def get_exhibit_animals(exhibit):
 
     results = curr.fetchone()
 
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_all_animals():
@@ -163,6 +173,10 @@ def get_all_animals():
     curr.execute("SELECT * FROM Animal")
 
     results = curr.fetchall()
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_all_shows():
@@ -173,6 +187,10 @@ def get_all_shows():
     results = curr.fetchall()
     for result in results:
         result['show_time'] = int(result['show_time'].timestamp())
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_all_visitors():
@@ -181,6 +199,10 @@ def get_all_visitors():
     curr.execute("SELECT username, email FROM `User` WHERE user_type = \"Visitor\"")
 
     results = curr.fetchall()
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_all_staff():
@@ -189,6 +211,10 @@ def get_all_staff():
     curr.execute("SELECT username, email FROM `User` WHERE user_type = \"Staff\"")
 
     results = curr.fetchall()
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
 
 def get_animal(name, species):
@@ -197,4 +223,42 @@ def get_animal(name, species):
     curr.execute("SELECT * FROM test.Animal WHERE animal_name=%s AND species=%s", (name, species))
 
     results = curr.fetchall()
+
+    conn.commit()
+    curr.close()
+    conn.close()
     return results
+
+#log
+def log_exhibit_visit(visitor_username, exhibit_name, visit_time):
+    conn, curr = connection()
+
+    curr.execute("INSERT INTO Visit_exhibit(visitor_username, exhibit_name, visit_time) VALUES (%s, %s, %s);",
+                 (visitor_username, exhibit_name, datetime.fromtimestamp(int(visit_time))))
+
+    conn.commit()
+    curr.close()
+    conn.close()
+    return "Successfully logged exhibit visit!!!"
+
+def log_show_visit(visitor_username, show_name, show_time):
+    conn, curr = connection()
+
+    curr.execute("INSERT INTO Visit_show(visitor_username, show_name, show_time) VALUES (%s, %s, %s);",
+                 (visitor_username, show_name, datetime.fromtimestamp(int(show_time))))
+
+    conn.commit()
+    curr.close()
+    conn.close()
+    return "Successfully logged show visit!!!"
+
+def log_note(staff_username, log_time, note, animal_name, animal_species):
+    conn, curr = connection()
+
+    curr.execute("INSERT INTO Note(staff_username, log_time, note, animal_name, animal_species) VALUES (%s, %s, %s, %s, %s);",
+                 (staff_username, datetime.fromtimestamp(int(log_time,)), note, animal_name, animal_species ) )
+
+    conn.commit()
+    curr.close()
+    conn.close()
+    return "Successfully added note"
